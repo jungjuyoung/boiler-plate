736914e429c391a6ce37cb0009190c80ee28ecdf
정주영 <nwwn1004@naver.com>
2020-10-01 03:23:05 +0900

    회원가입라우터 생성

--- server/index.js
+++ server/index.js
@@ -3,6 +3,13 @@ const app = express();
 const port = 5000;
 const mongoose = require('mongoose');
 const config = require('./config/key');
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+//application/json
+app.use(bodyParser.json());
 
 mongoose
   .connect(config.mongoURI, {
@@ -14,8 +21,23 @@ mongoose
   .then(() => console.log('MongoDB Connected...'))
   .catch((err) => console.log(err));
 
+// 최상위 라우터
 app.get('/', (req, res) => {
   res.send('Hello World! 안녕하세요');
+});
+
+// 회원가입 라우터
+app.post('/api/users/register', (req, res) => {
+  // 회원가입 할때 필요한 정보들을 client에서 가져오면
+  // 그것들을 DB에 넣어준다
+  // req.body안에는 json형식으로 bodyParser를 이용해서 client에서 보낸정보가 들어있다.
+  const user = new User(req.body);
+
+  // user.save는 몽고DB에서 가져온 메소드. user.save하면 User모델에 client에서 보낸정보(req.body)가 저장된다.
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
 });
 
 app.listen(port, () => {
