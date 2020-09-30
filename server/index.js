5ba5d64f18e3bc493511ae1fc538cc718bc1f40b
정주영 <nwwn1004@naver.com>
2020-10-01 02:20:48 +0900

    보일러플레이트 저장소에 올림

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+const mongoose = require('mongoose');
+const config = require('./config/key');
+
+mongoose
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch((err) => console.log(err));
+
+app.get('/', (req, res) => {
+  res.send('Hello World! 안녕하세요');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
