ed3b85bb65623fb27caf766d213e7b39436ada25
정주영 <nwwn1004@naver.com>
2020-11-15 04:00:20 +0900

    리덕스 로그인 완료

--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -1,10 +1,10 @@
-import Axios from 'axios';
+import axios from 'axios';
 import { LOGIN_USER } from './types';
 
 export function loginUser(dataTosubmit) {
-  const request = Axios.post('/api/users/login', dataTosubmit).then(
-    (res) => res.data
-  );
+  const request = axios
+    .post('/api/users/login', dataTosubmit)
+    .then((res) => res.data);
 
   return {
     type: LOGIN_USER,
