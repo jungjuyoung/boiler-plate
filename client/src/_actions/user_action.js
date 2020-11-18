c1cf14165d23266e68b94ed9055b4eb7ab903bb8
정주영 <nwwn1004@naver.com>
2020-11-19 00:52:50 +0900

    리덕스 회원가입

--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { LOGIN_USER } from './types';
+import { LOGIN_USER, REGISTER_USER } from './types';
 
 export function loginUser(dataTosubmit) {
   const request = axios
@@ -8,6 +8,16 @@ export function loginUser(dataTosubmit) {
 
   return {
     type: LOGIN_USER,
+    payload: request,
+  };
+}
+export function registerUser(dataTosubmit) {
+  const request = axios
+    .post('/api/users/register', dataTosubmit)
+    .then((res) => res.data);
+
+  return {
+    type: REGISTER_USER,
     payload: request,
   };
 }
--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,7 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { registerUser } from '../../../_actions/user_action';
 
-function RegisterPage() {
-  return <div>RegisterPage</div>;
+function RegisterPage(props) {
+  console.log(`props: ${props}`);
+
+  const dispatch = useDispatch();
+
+  const [Email, setEmail] = useState('');
+  const [Name, setName] = useState('');
+  const [Password, setPassword] = useState('');
+  const [ConfirmPassword, setConfirmPassword] = useState('');
+
+  const onEmailHandler = (e) => {
+    setEmail(e.currentTarget.value);
+  };
+
+  const onNamedHandler = (e) => {
+    setName(e.currentTarget.value);
+  };
+  const onPasswordHandler = (e) => {
+    setPassword(e.currentTarget.value);
+  };
+  const onConfirmPasswordHandler = (e) => {
+    setConfirmPassword(e.currentTarget.value);
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+
+    console.log('Email', Email);
+    console.log('Password', Password);
+
+    let body = {
+      email: Email,
+      name: Name,
+      password: Password,
+      confirmPassword: ConfirmPassword,
+    };
+
+    dispatch(registerUser(body)).then((res) => {
+      if (res.payload.success) {
+        props.history.push('/login');
+      } else {
+        alert('Failed to sign up');
+      }
+    });
+  };
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100vh',
+      }}
+    >
+      <form
+        onSubmit={onSubmitHandler}
+        style={{ display: 'flex', flexDirection: 'column' }}
+      >
+        <label>Email</label>
+        <input type="email" value={Email} onChange={onEmailHandler} />
+
+        <label>Name</label>
+        <input type="text" value={Name} onChange={onNamedHandler} />
+
+        <label>PassWord</label>
+        <input type="password" value={Password} onChange={onPasswordHandler} />
+
+        <label>Confirm PassWord</label>
+        <input
+          type="password"
+          value={ConfirmPassword}
+          onChange={onConfirmPasswordHandler}
+        />
+
+        <br />
+        <button onSubmit={onSubmitHandler}>회원가입</button>
+      </form>
+    </div>
+  );
 }
 
 export default RegisterPage;
