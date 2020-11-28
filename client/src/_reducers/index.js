a0427b7502abd81b6cc1287b8b659f8657219c98
정주영 <nwwn1004@naver.com>
2020-11-29 04:20:15 +0900

    리덕스 인증완료

--- client/src/_reducers/index.js
+++ client/src/_reducers/index.js
@@ -1,11 +1,7 @@
 import { combineReducers } from 'redux';
 import user from './user_reducer';
-// import register from './register_reducer';
-// import comment from './comment_reducer';
 
 const rootReducer = combineReducers({
   user,
-  // register,
-  // comment
 });
 export default rootReducer;
