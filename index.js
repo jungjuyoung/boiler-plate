92fa9fb678dfe451bf6012b4bfcbc7117fab953a
jungjuyoung <nwwn1004@naver.com>
2021-04-21 20:30:06 +0900

    feature: Encrypt to password with Bcrypt

--- index.js
+++ index.js
@@ -25,19 +25,23 @@ mongoose
 app.get('/', (req, res) => {
   res.send('Hello World!!!');
 });
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
 
 app.post('/register', (req, res) => {
   // 회원가입할때 필요한 정보들을 client에서 가져오면
+  // 비밀번호는 암호화 해서
   // 그것들을 데이터베이스에 넣어준다.
   const user = new User(req.body);
+  console.log(`index.js register router user: ${user}`);
   user.save((err, userInfo) => {
+    console.log(
+      `index.js register router user.save user: ${user}, userInfo: ${userInfo}`
+    );
     if (err) return res.json({ success: false, err });
     return res.status(200).json({
       success: true,
     });
   });
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
 });
--- models/User.js
+++ models/User.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
+console.log(`bcrypt: ${JSON.stringify(bcrypt)}`);
 const userSchema = mongoose.Schema({
   name: {
     type: String,
@@ -29,6 +32,27 @@ const userSchema = mongoose.Schema({
   tokenExp: {
     type: Number,
   },
+});
+
+userSchema.pre('save', function (next) {
+  var user = this;
+  // 비밀번호를 변경할 때만
+
+  if (user.isModified('password')) {
+    // 비밀전호를 암호화 한다
+    console.log('password changed');
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+      if (err) return next(err);
+      console.log(`salt: ${salt}, user: ${user}`);
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
 });
 
 const User = mongoose.model('User', userSchema);
