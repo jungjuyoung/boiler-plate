f162f6252043bdd6880a288a1b7d306c72c2de2d
jungjuyoung <nwwn1004@naver.com>
2021-04-23 21:59:13 +0900

    feature: react-router install and setting

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import LadingPage from './components/views/LandingPage/LandingPage';
+import Loginpage from './components/views/LoginPage/LoginPage';
+import Register from './components/views/Registerpage/Registerpage';
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route exact path="/" component={LadingPage} />
+          <Route exact path="/login" component={Loginpage} />
+          <Route exact path="/" component={Register} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
