b7fbd308803e049a8032fcb361425c4e96a4a404
jungjuyoung <nwwn1004@naver.com>
2021-04-25 02:11:34 +0900

    feature: redux install and connected

---
+++ client/src/_reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+// import user form './user_reducer
+// import comment form './comment_reducer
+
+const rootReducer = combineReducers({
+  //  user,
+  // comment
+});
+export default rootReducer;
