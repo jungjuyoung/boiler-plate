2a77008166b43021a85732d4f16c99fd7248a170
jungjuyoung <nwwn1004@naver.com>
2021-04-26 16:44:37 +0900

    feature: Register page

--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { LOGIN_USER } from './types';
+import { LOGIN_USER, REGISTER_USER } from './types';
 
 export function loginUser(dataToSubmit) {
   const request = axios
@@ -7,4 +7,12 @@ export function loginUser(dataToSubmit) {
     .then((res) => res.data);
   // return으로 리듀서에 현재 state와 액션을 전달
   return { type: LOGIN_USER, payload: request };
+}
+
+export function registerUser(dataToSubmit) {
+  const request = axios
+    .post('/api/users/register', dataToSubmit)
+    .then((res) => res.data);
+  // return으로 리듀서에 현재 state와 액션을 전달
+  return { type: REGISTER_USER, payload: request };
 }
--- client/src/components/views/Registerpage/Registerpage.js
+++ client/src/components/views/Registerpage/Registerpage.js
@@ -1,7 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { registerUser } from '../../../_actions/user_action';
 
-function Registerpage() {
-  return <div>Registerpage</div>;
+function Registerpage(props) {
+  const dispatch = useDispatch();
+
+  const [Email, setEmail] = useState('');
+  const [Name, setName] = useState('');
+  const [Password, setPassword] = useState('');
+  const [ConfirmPassword, setConfirmPassword] = useState('');
+
+  const onEmailHandler = (e) => {
+    console.log(`e: ${e.currentTarget.value}`);
+    setEmail(e.currentTarget.value);
+  };
+  const onNameHandlerHandler = (e) => {
+    console.log(`e: ${e.currentTarget.value}`);
+    setName(e.currentTarget.value);
+  };
+  const onPasswordHandler = (e) => {
+    console.log(`e: ${e.currentTarget.value}`);
+    setPassword(e.currentTarget.value);
+  };
+  const onConfirmPasswordHandler = (e) => {
+    console.log(`e: ${e.currentTarget.value}`);
+    setConfirmPassword(e.currentTarget.value);
+  };
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(`Email: ${Email}`);
+    console.log(`Password: ${Password}`);
+
+    if (Password !== ConfirmPassword) {
+      return alert('비밀번호와 비밀번호 확인은 같아야 합니다.');
+    }
+
+    const body = {
+      email: Email,
+      name: Name,
+      password: Password,
+      confirmPassword: ConfirmPassword,
+    };
+
+    dispatch(registerUser(body)).then((res) => {
+      console.log(`dispatch res: ${JSON.stringify(res.payload)}`);
+      if (res.payload.success) {
+        props.history.push('/login');
+      } else {
+        alert('Failed to sign up!!!');
+      }
+    });
+    // 원래는 여기서 axios.post('/api/users/login', body).then(res)로
+    // 서버에 전달해야 하는데 리덕스를 사용하기때문에 dispatch로 대체
+  };
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100vh',
+      }}
+    >
+      <form
+        style={{ display: 'flex', flexDirection: 'column' }}
+        onSubmit={onSubmitHandler}
+      >
+        <label>Email</label>
+        <input type="email" value={Email} onChange={onEmailHandler} />
+        <label>Name</label>
+        <input type="text" value={Name} onChange={onNameHandlerHandler} />
+        <label>Password</label>
+        <input type="password" value={Password} onChange={onPasswordHandler} />
+        <label>Confirm Password</label>
+        <input
+          type="password"
+          value={ConfirmPassword}
+          onChange={onConfirmPasswordHandler}
+        />
+
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default Registerpage;
